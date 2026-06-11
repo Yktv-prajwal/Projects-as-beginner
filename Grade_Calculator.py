ced047d51d37d5b42e0735986ddfbaeb08600127
Prajwal Kumbhar <prajwalkumbhar28@gmail.com>
2026-06-11 19:16:09 +0530

    Python Project That Calculating Students Grade

---
+++ Grade_Calculator.py
@@ -0,0 +1,18 @@
+a=int(input("Enter Your Marks:"))
+
+if  a>=90 and a==100:
+    print("You Got A+ Grade For The Marks :",a)
+elif a>=80 and a<=89:
+    print("You Got A Grade For The Marks :",a)
+elif a>=70 and a<=79:
+    print("You Got B+ Grade For The Marks :",a)
+elif a>=60 and a<=69:
+    print("You Got B Grade For The Marks :",a)
+elif a>=50 and a<=59:
+    print("You Got C+ Grade For The Marks :",a)
+elif a>=40 and a<=49:
+    print("You Got C Grade For The Marks :",a)
+
+else:
+    print("You Are Fail By Obtaining Marks:",a)
+
